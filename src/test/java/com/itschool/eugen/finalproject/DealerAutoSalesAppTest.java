0e472addae8ab639e7ac6561e7bdb73d84f2e502
eugenstan <d.eugen.stan@gmail.com>
2024-08-15 15:58:59 +0300

    Insert Test in Final Project

---
+++ src/test/java/com/itschool/eugen/finalproject/DealerAutoSalesAppTest.java
@@ -0,0 +1,13 @@
+package com.itschool.eugen.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DealerAutoSalesAppTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
+
